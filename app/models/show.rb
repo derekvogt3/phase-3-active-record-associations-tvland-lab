55f1742aba27f26bf82eecd2ffd2a255089ed97d
Derek Vogt <derekvogt3@gmail.com>
2022-07-25 16:27:26 -0400

    commit tree ac2da25fb59c36b61a850b1940b833302c708127

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,26 @@
 class Show < ActiveRecord::Base
+
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+
+    def actors_list
+        array = Array.new
+
+        self.actors.each do |character|
+
+            array.push(character.first_name+" "+character.last_name)
+
+            
+        end
+
+        array
+        
+    end
+
+
+
+
   
 end
